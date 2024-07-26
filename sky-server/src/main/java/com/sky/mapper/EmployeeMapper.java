5bd32873f4fcf514e5a4b9295529d3f271d1e07e
imAnswer <857893946@qq.com>
2024-07-26 23:34:38 +0800

    新增分类管理功能

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -24,7 +24,7 @@ public interface EmployeeMapper {
 
     @Insert("insert into employee(name,username,password,phone,sex,id_number,create_time,update_time,create_user,update_user,status) " +
             "values" +
-            "(#{name}),#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status}")
+            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void addEmployee(Employee employee);
 
     Page pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
